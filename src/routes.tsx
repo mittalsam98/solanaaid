5fde1d38313a0c7ce11ab5b58940436cdfe84313
Sachin <mittalsam98@gmail.com>
2024-09-09 14:13:29 +0530

    Added basic template for shadcn ui and react

---
+++ src/routes.tsx
@@ -0,0 +1,12 @@
+import { Navigate, Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import { Dashboard } from './pages/Dashboard';
+
+export default function AppRoutes() {
+  return (
+    <Router>
+      <Routes>
+        <Route path='/' element={<Dashboard />} />
+      </Routes>
+    </Router>
+  );
+}
